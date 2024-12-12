5d5c1a629cca12e0e441f90402dba56f37a9d2ad
Prabal Gupta <er.prabalgupta.2411@gmail.com>
2024-12-12 21:30:14 +0530

    deleted:    frontend/src/pages/InitiativePage.js 	modified:   frontend/src/pages/PrivacyPolicy.js 	modified:   frontend/src/pages/RefundCancellationPolicy.js 	modified:   frontend/src/pages/TermsOfUse.js

--- frontend/src/pages/PrivacyPolicy.js
+++ frontend/src/pages/PrivacyPolicy.js
@@ -5,14 +5,14 @@ const PrivacyPolicy = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center py-8 px-4">
       <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-lg p-6">
-        <h1 className="text-4xl font-bold text-center text-blue-600 mb-6">Privacy Policy</h1>
+        <h1 className="text-4xl font-bold text-center text-[#3b6572] mb-6">Privacy Policy</h1>
         
-        <p className="mb-6 text-gray-700 leading-relaxed">
+        <p className="mb-6 text-black-700 leading-relaxed">
           ALIVE FOUNDATION is committed to the ethical collection, retention, and use of information on this site www.alivefoundation.in (‘Site’) that you provide to us about yourself (‘Personal Information’).
         </p>
         
-        <h2 className="text-3xl font-semibold text-gray-800 mt-6 mb-4">Your Personal Information</h2>
-        <ul className="list-disc ml-5 mb-6 text-gray-700">
+        <h2 className="text-3xl font-semibold text-[#6a8f9a] mt-6 mb-4">Your Personal Information</h2>
+        <ul className="list-disc ml-5 mb-6 text-black-700">
           <li>Name</li>
           <li>Age</li>
           <li>Occupation</li>
@@ -23,24 +23,24 @@ const PrivacyPolicy = () => {
           <li>Any other data the website may require</li>
         </ul>
 
-        <p className="mb-6 text-gray-700 leading-relaxed">
+        <p className="mb-6 text-black-700 leading-relaxed">
           The following Privacy Policy sets out our understanding with you on the collection, use, and security of your personal information. Please read the complete Privacy Policy.
         </p>
 
-        <p className="mb-6 text-gray-700 leading-relaxed">
+        <p className="mb-6 text-black-700 leading-relaxed">
           Your use of the Website constitutes your consent to and shall be bound by all the terms and conditions contained in this Privacy Policy (as amended from time to time).
         </p>
 
-        <h2 className="text-3xl font-semibold text-gray-800 mt-6 mb-4">Collection of Information</h2>
+        <h2 className="text-3xl font-semibold text-[#6a8f9a] mt-6 mb-4">Collection of Information</h2>
         <p className="mb-6 text-gray-700 leading-relaxed">
           You browse the site anonymously. We do not require you to identify yourself or reveal any personal information when browsing through the Site. However, you may not be able to access certain sections of the Site or interact with us without providing personal information.
         </p>
 
-        <h2 className="text-3xl font-semibold text-gray-800 mt-6 mb-4">Use of Personal Information</h2>
-        <p className="mb-6 text-gray-700 leading-relaxed">
+        <h2 className="text-3xl font-semibold text-black-800 mt-6 mb-4">Use of Personal Information</h2>
+        <p className="mb-6 text-black-700 leading-relaxed">
           ALIVE FOUNDATION will use Personal Information for internal purposes including the following:
         </p>
-        <ul className="list-disc ml-5 mb-6 text-gray-700">
+        <ul className="list-disc ml-5 mb-6 text-black-700">
           <li>Sending you internal emails, features, promotional materials, surveys, and updates.</li>
           <li>Processing your donation.</li>
           <li>Receipt of donations made by you to ALIVE FOUNDATION.</li>
--- frontend/src/pages/TermsOfUse.js
+++ frontend/src/pages/TermsOfUse.js
@@ -3,19 +3,19 @@ import React from "react";
 
 const TermsOfUse = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center py-8 px-4">
+    <div className="bg-[#F8FFFF] min-h-screen flex flex-col justify-center py-8 px-4">
       <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-lg p-6">
-        <h1 className="text-4xl font-bold text-center text-blue-600 mb-6">Terms of Use</h1>
+        <h1 className="text-4xl font-bold text-center text-[#3b6572] mb-6">Terms of Use</h1>
         
         <p className="mb-6 text-gray-700 leading-relaxed">
-          Welcome to <a href="https://www.alivefoundation.in" className="text-blue-500 underline" target="_blank" rel="noopener noreferrer">www.alivefoundation.in</a>. If you continue to browse and use this website, you agree to be bound by and are bound by the following terms and conditions of use, which, along with our Privacy Policy, govern your relationship with ALIVE FOUNDATION concerning this website.
+          Welcome to <a href="https://www.alivefoundation.in" className="text-[#3b6572] underline" target="_blank" rel="noopener noreferrer">www.alivefoundation.in</a>. If you continue to browse and use this website, you agree to be bound by and are bound by the following terms and conditions of use, which, along with our Privacy Policy, govern your relationship with ALIVE FOUNDATION concerning this website.
         </p>
 
-        <h2 className="text-3xl font-semibold text-gray-800 mt-6 mb-4">Terms of Use</h2>
-        <p className="mb-4 text-gray-700 leading-relaxed">
+        <h2 className="text-3xl font-semibold text-[#6a8f9a] mt-6 mb-4">Terms of Use</h2>
+        <p className="mb-4 text-black-700 leading-relaxed">
           Your use of this website is subject to the following terms of use:
         </p>
-        <ul className="list-disc ml-5 mb-6 text-gray-700 leading-relaxed">
+        <ul className="list-disc ml-5 mb-6 text-black-700 leading-relaxed">
           <li>The contents and data of the pages of this website are for your general information and use only. It is subject to change without any prior notice.</li>
           <li>Neither we nor any third party make any warranties or guarantees about the accuracy, timeliness, performance, completeness, or suitability of the information on this website.</li>
           <li>Use of any information or material on this website is entirely at your own risk, for which we will not be liable.</li>
